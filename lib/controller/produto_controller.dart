c931481cad2a293cc37c94f2e5b5d43a2e60fc97
Rafinha003 <gabifpsoares@gmail.com>
2025-03-18 19:36:29 -0300

    1

---
+++ lib/controller/produto_controller.dart
@@ -0,0 +1,31 @@
+//
+// controller > produto_controller.dart
+//
+import 'package:flutter/material.dart';
+
+import '../model/produto_model.dart';
+
+class ProdutoController extends ChangeNotifier {
+  final List<Produto> _produtos = [
+    Produto(nome: 'Notebook', preco: 4800.00),
+    Produto(nome: 'Mouse', preco: 90.00),
+    Produto(nome: 'Teclado', preco: 220.00),
+    Produto(nome: 'Monitor', preco: 650.00),
+    Produto(nome: 'Smartphone', preco: 3200.00),
+  ];
+
+  bool _visualizarLista = true;
+
+  List<Produto> get produtos => _produtos;
+  bool get visualizarLista => _visualizarLista;
+
+  void alterarVisualizacao(valor) {
+    _visualizarLista = valor;
+    notifyListeners();
+  }
+
+  void removerItem(index) {
+    _produtos.removeAt(index);
+    notifyListeners();
+  }
+}
